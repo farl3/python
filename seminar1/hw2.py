a57179d69e58b6e239180398517126efb30c802c
farl3 <vd.av@ya.ru>
2022-12-19 19:31:37 +0300

    семинар1

--- seminar1/hw2.py
+++ seminar1/hw2.py
@@ -14,3 +14,16 @@ for x in [0, 1]:
             p1 = not (x or y or z)
             p2 = not x and not y and not z
             print(p1 == p2)
+
+
+
+# 2. Напишите программу для проверки ложности утверждения
+# (W ⋀ Z) ⋁ ¬Y ⋁ (¬X ≡ ¬W) для всех значений предикат.
+
+# print('x','y','z', 'w' )
+# for x in range (2):
+#     for y in range (2):
+#         for z in range (2):
+#             for w in range (2):
+#                 if not ((w and z) or not y or (not x == (not w))):
+#                     print(x, y, z, w)
